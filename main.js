c418f76ca328c4bb9cec2a48231c81fc242d26f0
Scott Schwartz <scottschwartz97@gmail.com>
2024-09-18 23:57:50 -0400

    findWordInDictionary() works but i am changing the api I want to use

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-let firstTimeRun = true;
-
-function run() {
-    console.log("Here we go...");
-
-    const time = getTime();
-        if(!time) return;
-    console.log(time);
-
-    let currString = "";
-    for(let i = 0; i < 3; i++)
-    {
-        currString = currString + getRandomLetter(26);
-    }
-    console.log(currString);
-}
-
 let getTime = () => {
-    const input = document.getElementById("time");
-    return input.value;
+  const input = document.getElementById("time");
+  return input.value;
+};
+
+let getRandomLetter = (n) => {
+  return String.fromCharCode(Math.floor(Math.random() * n) + 97);
+};
+
+async function findWordInDictionary(wordToCheck) {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${wordToCheck}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not find word");
+    }
+
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
-let getRandomLetter = (n) => {return String.fromCharCode(Math.floor(Math.random() * n) + 97)};
+async function run() {
+  console.log("Here we go...");
+
+  const time = getTime();
+  // if (!time) return;
+
+  let notWord = false;
+
+  while (!notWord) {
+    let currWord = "";
+
+    for (let i = 0; i < 3; i++) {
+      currWord += getRandomLetter(26);
+    }
+
+    notWord = await findWordInDictionary(currWord);
+    console.log(currWord);
+    console.log(notWord);
+  }
+
+  console.log("YOU WIN");
+}
+
+run();
